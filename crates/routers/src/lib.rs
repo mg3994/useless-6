0b0213bff24a43c0b8eefcf8f29b1bf1fa2bb300
mg3994 <manishgautammg7@yahoo.com>
2026-03-20 08:40:59 -0700

    second useful commit

--- crates/routers/src/lib.rs
+++ crates/routers/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use rust_embed::RustEmbed;
+use salvo::prelude::*;
+use salvo::serve_static::{static_embed, EmbeddedFileExt};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(RustEmbed)]
+#[folder = "../../assets"]
+struct Assets;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn root() -> Router {
+    let favicon = Assets::get("favicon.ico")
+        .expect("favicon not found")
+        .into_handler();
+    let router = Router::new()
+        .hoop(Logger::new())
+        // ....
+        .push(Router::with_path("favicon.ico").get(favicon))
+        .push(Router::with_path("assets/{**rest}").get(static_embed::<Assets>()));
+    let doc = OpenApi::new("salvo web api", "0.0.1").merge_router(&router);
+    router
+        .unshift(doc.into_router("/api-doc/openapi.json"))
+        .unshift(Scalar::new("/api-doc/openapi.json").into_router("scalar"))
 }
